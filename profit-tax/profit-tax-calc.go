b6ad57cdb5c55b2f47fe65013c7037c3a888eb64
SwadhiSankar <swadhis14@gmail.com>
2024-09-02 13:22:14 +0530

    profit-tax-cal ( Printf scan formatting string )

---
+++ profit-tax/profit-tax-calc.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main(){
+	var revenue, expenses, taxRate float64
+
+	fmt.Print("Revenue")
+	fmt.Scan(&revenue)
+
+	fmt.Print("expenses")
+	fmt.Scan(&expenses)
+
+	fmt.Print("taxRate")
+	fmt.Scan(&taxRate)
+
+	EBT := revenue-expenses
+	profit := EBT * (1-taxRate/100)
+	ratio := EBT/profit
+	
+	fmt.Printf("Profit :  %v\n",profit)
+	fmt.Print(EBT)
+	fmt.Print(ratio)
+
+
+
+
+}
