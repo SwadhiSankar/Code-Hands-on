62f62c09aac0ca4420860ca05fd5b3d25686d7c9
SwadhiSankar <swadhis14@gmail.com>
2024-09-02 18:32:28 +0530

    error handling and using sprint function to write in a new file

--- profit-tax/profit-tax-calc.go
+++ profit-tax/profit-tax-calc.go
@@ -1,12 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
 
 func main(){
 	
-    revenue := getUserInput("Revenue: ")
-	expenses := getUserInput("Expenses :")
-	taxRate := getUserInput("TaxRate : ")
+    revenue,err1 := getUserInput("Revenue: ")
+	
+	expenses,err2 := getUserInput("Expenses :")
+	
+	taxRate ,err3 := getUserInput("TaxRate : ")
+	if err1 != nil || err2 !=nil || err3 != nil{
+		fmt.Println(err1)
+		return
+	}
 
 	EBT, profit, ratio :=CalculateFinancials(revenue,expenses,taxRate)
 	
@@ -14,9 +25,14 @@ func main(){
 	fmt.Printf("%.1f\n",EBT)
 	fmt.Printf("%.1f\n",profit)
 	fmt.Printf("%.3f\n",ratio)
+	storeResults(EBT,profit,ratio)
 
 }
 
+func storeResults (EBT, profit,ratio float64 ){
+	results := fmt.Sprintf("EBT : %.1f\n Profit :%.1f \n Ratio: %.3f \n",EBT, profit,ratio)
+	os.WriteFile("results.txt",[]byte (results), 0644)
+}
 func CalculateFinancials(revenue, expenses, taxRate float64) (float64,float64,float64){
 
 	EBT := revenue-expenses
@@ -25,9 +41,12 @@ func CalculateFinancials(revenue, expenses, taxRate float64) (float64,float64,fl
 	return EBT,profit,ratio
 }
 
-func getUserInput(Text string)float64{
+func getUserInput(Text string)(float64,error){
 	var userInput float64
 	fmt.Print(Text)
 	fmt.Scan(&userInput)
-	return userInput
+	if userInput <=0{
+		return 0,errors.New("Value should not be less than 0")
+	}
+	return userInput, nil
 }
